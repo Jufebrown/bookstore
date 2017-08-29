9538f194025a187abe1404b5f046500f72fd1acb
Jufebrown <jufebrown@gmail.com>
2017-08-28 20:21:59 -0500

    added product types and radio buttons

--- js/main.js
+++ js/main.js
@@ -1,65 +1,74 @@
-var products = [
-  {
-    "id": 1,
-    "name": "Harry Potter and the Half-Blood Prince",
-    "author": "J.K. Rowlings",
-    "picture_url": "http://img.timeinc.net/time/2007/harry_potter/107_potter_tout.jpg",
-    "price": 100,
-    "selling_points": [
-      "It’s become essentially a rite of passage into the adult world.",
-      "The essential guide to wizardry.",
-      "Ain't no such thing as halfway crooks. -Mob Deep"
-    ]
-  },
-  {
-    "id": 2,
-    "name": "I Can't Make This Up: Life Lessons",
-    "author": "Kevin Hart",
-    "picture_url": "https://prodimage.images-bn.com/pimages/9781501155581_p0_v2_s550x406.jpg",
-    "price": 25,
-    "selling_points": [
-      "You like cheese without the corners, in other words you’ll never be a slice. -Kevin Hart",
-      "Laughter is Good for the Soul",
-      "Because Kevin Hart is HILARIOUS"
-    ]
-  },
-  {
-    "id": 3,
-    "name": "Book Three",
-    "author": "Savvy Coders",
-    "picture_url": "http://pngimg.com/uploads/book/book_PNG2111.png",
-    "price": 199,
-    "selling_points": [
-      "This book is hardcover",
-      "This is a best seller",
-      "This book will make you money"
-    ]
-  },
-  {
-    "id": 4,
-    "name": "18 months",
-    "author": "Calvin Harris",
-    "picture_url": "https://bethlemondinea2media2014.files.wordpress.com/2014/12/calvin-harris-18-months-album-cover-i9.png",
-    "price": 9.99,
-    "selling_points": [
-      "a life long project from award winning artist DJ Calvin Harris",
-      "Summer has never been hotter with jams this sweet",
-      "Good vibrations for every occasion"
-    ]
-  },
-  {
-    "id": 5,
-    "name": "Anti",
-    "author": "Rihanna",
-    "picture_url": "https://upload.wikimedia.org/wikipedia/en/3/32/Rihanna_-_Anti.png",
-    "price": 10,
-    "selling_points": [
-      "She's Crazy, Sexy, and Cool",
-      "Unique Sound",
-      "Top Charted Artist"
-    ]
-  }
-]
+var products = {
+  books: [
+    {
+      "id": 1,
+      "productType": "books",
+      "name": "Harry Potter and the Half-Blood Prince",
+      "author": "J.K. Rowlings",
+      "picture_url": "http://img.timeinc.net/time/2007/harry_potter/107_potter_tout.jpg",
+      "price": 100,
+      "selling_points": [
+        "It’s become essentially a rite of passage into the adult world.",
+        "The essential guide to wizardry.",
+        "Ain't no such thing as halfway crooks. -Mob Deep"
+      ]
+    },
+    {
+      "id": 2,
+      "productType": "books",
+      "name": "I Can't Make This Up: Life Lessons",
+      "author": "Kevin Hart",
+      "picture_url": "https://prodimage.images-bn.com/pimages/9781501155581_p0_v2_s550x406.jpg",
+      "price": 25,
+      "selling_points": [
+        "You like cheese without the corners, in other words you’ll never be a slice. -Kevin Hart",
+        "Laughter is Good for the Soul",
+        "Because Kevin Hart is HILARIOUS"
+      ]
+    },
+    {
+      "id": 3,
+      "productType": "books",
+      "name": "Book Three",
+      "author": "Savvy Coders",
+      "picture_url": "http://pngimg.com/uploads/book/book_PNG2111.png",
+      "price": 199,
+      "selling_points": [
+        "This book is hardcover",
+        "This is a best seller",
+        "This book will make you money"
+      ]
+    }
+  ],
+  music : [
+    {
+      "id": 4,
+      "productType": "music",
+      "name": "18 months",
+      "author": "Calvin Harris",
+      "picture_url": "https://bethlemondinea2media2014.files.wordpress.com/2014/12/calvin-harris-18-months-album-cover-i9.png",
+      "price": 9.99,
+      "selling_points": [
+        "a life long project from award winning artist DJ Calvin Harris",
+        "Summer has never been hotter with jams this sweet",
+        "Good vibrations for every occasion"
+      ]
+    },
+    {
+      "id": 5,
+      "productType": "music",
+      "name": "Anti",
+      "author": "Rihanna",
+      "picture_url": "https://upload.wikimedia.org/wikipedia/en/3/32/Rihanna_-_Anti.png",
+      "price": 10,
+      "selling_points": [
+        "She's Crazy, Sexy, and Cool",
+        "Unique Sound",
+        "Top Charted Artist"
+      ]
+    }
+  ]
+}
 
 
 
@@ -90,10 +99,12 @@ var addToPage = function(productObj) {
 // }
 
 var populatePage = function() {
-  products.forEach(function(product) {
-    addToPage(product)
-  })
-}
+  for(var productType in products) {
+    for (var i = 0; i < products[productType].length; i++) {
+      addToPage(products[productType][i]);
+    };
+  };
+};
 
 populatePage()
 
@@ -116,11 +127,17 @@ populatePage()
 var formToObject = function(form) {
   var data = $(form).serializeArray();
   var formObject = {};
+  var productArray
 
-  formObject.id = products.length++; // measures the length of the global products array
+
+  // formObject.id = products.length++; // measures the length of the global products array
   data.forEach( function(field){
       formObject[field.name] = field.value;
   } );
+
+  productArray = products[formObject.type]
+  formObject.id = productArray.length + 1
+
   return formObject
 }
 
@@ -129,13 +146,14 @@ var emptyContent = function() {
 }
 
 var addFormObjToProducts = function(formObject) {
-  products.push(formObject); // adds the new product to the global products array
+  var productType = formObject.type;
+  products[productType].push(formObject); // adds the new product to the global products array
 }
 
 
 $("form").on("submit", function(event) {
     event.preventDefault();
-    
+
     var formObject = formToObject(this)
     emptyContent()
     addFormObjToProducts(formObject)
